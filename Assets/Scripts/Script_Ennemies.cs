5ebfce347736f3acb02c65e96206647fce51768a
MSI\Lucas <lucasblon@orange.fr>
2019-11-26 13:48:38 +0100

    j'ai tenté un truc mais ca ma soulé

---
+++ Assets/Scripts/Script_Ennemies.cs
@@ -0,0 +1,32 @@
+﻿    using UnityEngine;
+
+public class Script_Ennemies : MonoBehaviour
+{
+
+    public float health = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage (float amount)
+    {
+        health -= amount;
+        if (health <=0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Script_Weapon.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Script_Weapon : MonoBehaviour
+{
+    public GameObject _bullet;
+    public float _speed = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire 1"))
+        {
+            Shoot();
+        }
+
+
+    }
+
+
+    void Shoot ()
+    {
+        GameObject _instBullet = Instantiate(_bullet,transform.position,Quaternion.identity) as GameObject;
+        Rigidbody _instBulletRigidbody = _instBullet.GetComponent<Rigidbody>();
+        _instBulletRigidbody.AddForce(Vector3.forward * _speed);
+    }
+}
+
